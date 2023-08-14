09c3e68c081cf9df14fe04c16c0ec1d52993ac62
Feliperto <fperezferraro@gmail.com>
2023-08-14 18:09:07 -0300

    Revert "updated the project"
    This reverts commit a201f7cf25de5197686d2c6f09d1e3cacd608c2a.

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
 );
