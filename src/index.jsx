a201f7cf25de5197686d2c6f09d1e3cacd608c2a
Feliperto <fperezferraro@gmail.com>
2023-08-14 17:00:19 -0300

    updated the project

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 );
